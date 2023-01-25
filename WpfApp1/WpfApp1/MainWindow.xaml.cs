ad1d34b47d38cde9478b238da209907f890a130e
mskvszkyt <71025319+mskvszkyt@users.noreply.github.com>
2023-01-25 19:11:09 +0100

    Add files via upload

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Click_Osszead(object sender, RoutedEventArgs e)
+        {
+            String eredmeny = $"{txtAszam.Text} + {txtBszam.Text} = {Convert.ToDouble(txtAszam.Text) + Convert.ToDouble(txtBszam.Text)}";
+            MessageBox.Show("Az összeadás eredménye: "+eredmeny);
+        }
+
+        private void Click_Kivon(object sender, RoutedEventArgs e)
+        {
+            String eredmeny = $"{txtAszam.Text} - {txtBszam.Text} = {Convert.ToDouble(txtAszam.Text) - Convert.ToDouble(txtBszam.Text)}";
+            MessageBox.Show("A kivonás eredménye: " + eredmeny);
+        }
+
+        private void Click_Szoroz(object sender, RoutedEventArgs e)
+        {
+            String eredmeny = $"{txtAszam.Text} * {txtBszam.Text} = {Convert.ToDouble(txtAszam.Text) * Convert.ToDouble(txtBszam.Text)}";
+            MessageBox.Show("A szorzás eredménye: " + eredmeny);
+        }
+
+        private void Click_Oszt(object sender, RoutedEventArgs e)
+        {
+            String eredmeny = $"{txtAszam.Text} / {txtBszam.Text} = {Convert.ToDouble(txtAszam.Text) / Convert.ToDouble(txtBszam.Text)}";
+            MessageBox.Show("Az osztás eredménye: " + eredmeny);
+        }
+    }
+}
